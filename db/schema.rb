8e20d8a4fdf9573a305b204c42e141d03b7014e0
SooyeonJeung <jeungsooyeon@gmail.com>
2021-03-12 15:10:44 +1100

    finished configuring devise for username field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_034717) do
+ActiveRecord::Schema.define(version: 2021_03_12_040251) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_034717) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
