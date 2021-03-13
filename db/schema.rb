c5057ebe1ee64ab079c3d3bcb2cf1df5860795d9
SooyeonJeung <jeungsooyeon@gmail.com>
2021-03-13 13:42:17 +1100

    started heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_12_040251) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "category"
