faa7359fe990ee21d80b5f3616fad053b8807bb6
SooyeonJeung <jeungsooyeon@gmail.com>
2021-03-18 22:16:55 +1100

    updated pages

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,11 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.where.not(id: current_user.id)
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+    p "here:"
+    p @conversations
   end
 
   def create
